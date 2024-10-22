48ce9b1ae007b21bcbd8f3b371ee7fe30dcfa553
atchman <atchman@users.noreply.github.com>
2024-10-23 00:04:43 +0200

    day2 part 1 solved diffrent approach than at the beginning any()

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # aoc-2023
 Advent of Code 2023
 
-- [x] Day 1 Part 1 solved
-- [x] Day 1 Part 2 solved
+- [x] Day 1 solved
+- [x] Day 2 Part 1 solved
+- [ ] Day 2 Part 2 
 
--- src/day02.py
+++ src/day02.py
@@ -10,53 +10,42 @@ PATH = os.path.abspath(REL_PATH)
 def main():
     part_one = part_1()
     print(f"Part 1: Summe {part_one}")
-
     #part_two = part_2()
     #print(f"Part 2: Summe {part_two}")
 
  
 def part_1():
     total = []
-    p = [12, 13, 14]
-    
+
     with open(PATH, 'r') as FILE:
         for LINE in FILE: 
-            game_id, subset_list = parse_game(LINE)
-            print(f"game: {game_id}")
-            print(f"subset: {subset_list}")
-            r, g, b = 0, 0, 0
-            for i in range(len(subset_list)):
-                r = r + subset_list[i][0]
-                g = g + subset_list[i][1]
-                b = b + subset_list[i][2]
-            if r < p[0]+1:
-                if g < p[1]+1:
-                    if b < p[2]+1:
-                        print(f"Tue")
-                        total.append(int(game_id))
-    print(f"total {total}")
+            game_id, subset = LINE.split(':', 1)
+            game_id = game_id.split(' ')[1]
+            subset = subset.strip().split('; ')
+            
+            possible = True
+            map = { "red": 12, "green": 13, "blue": 14 }
+            for i in subset:
+                bag = i.split(', ')
+                for item in bag: 
+                    num, item_str  = item.split(' ')
+                    if item_str == 'red':
+                        map["red"] = map.get('red') - int(num)
+                    if item_str == 'green':
+                        map["green"] = map.get("green") - int(num)
+                    if item_str == 'blue':
+                        map["blue"] = map.get("blue") - int(num)
+                    
+                    # check if still in parameters
+                    if any(value < 0 for value in map.values()):
+                        possible = False
+                        break
+                if not possible:
+                    break
+            if possible:
+                total.append(int(game_id))
     return sum(total)
-
-
-def parse_game(LINE):
-    subset_list = [0, 0, 0]
-    game_id, subset = LINE.split(':', 1)
-    game_id = game_id.split(' ')[1]
-    subset = subset.strip().split('; ')
-    
-    for i in range(len(subset)):
-        bag = subset[i].split(', ')
-        # 0 = red, 1, green, 2, blue
-        subset_list.append([0, 0, 0])
-        for x in range(len(bag)):
-            if bag[x].find("red") != -1:
-                subset_list[i][0] = int(bag[x].split(' ', 1)[0].strip())
-            if bag[x].find("green") != -1:
-                subset_list[i][1] = int(bag[x].split(' ', 1)[0].strip())
-            if bag[x].find("blue") != -1:
-                subset_list[i][2] = int(bag[x].split(' ', 1)[0].strip())
-    return game_id, subset_list
-
+                
         
 if __name__ == '__main__':
     main()
